557add4aa7e32a6de1bcb1a4ff8316fa9f99aa7c
carderm <michaelcard_GitHub@outlook.com>
2020-05-23 18:24:37 +1000

    ffs packaging

--- setup.py
+++ setup.py
@@ -1,3 +1,18 @@
 from setuptools import setup
 
-setup()
+setup(install_requires = ['beautifulsoup4',
+                    'certifi',
+                    'chardet',
+                    'Django<2.3',
+                    'django-crispy-forms',
+                    'django-sekizai',
+                    'django-versionfield',
+                    'idna',
+                    'pyparsing',
+                    'pytz',
+                    'requests',
+                    'six',
+                    'soupsieve',
+                    'sqlparse',
+                    'urllib3',
+                   ])
